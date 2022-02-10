460badab9d5db803352d54859b904cf93d05fec0
Faraknights <55319852+Faraknights@users.noreply.github.com>
2022-02-10 01:04:35 +0100

    Création du projet - fichiers de base

---
+++ src/Mapile.java
@@ -0,0 +1,17 @@
+/**
+ * La classe Mapile permet l'execution de la machine a pile virtuelle MAPILE
+ *  
+ * @author Girard, Masson, Perraudeau
+ *
+ */
+ 
+public class Mapile  {
+ 
+
+    // necessite les classes ExecMapile.class (dans libClass_projet) et Mnemo.class 
+    
+    public static void main (String[] args) {
+    	ExecMapile.activer();
+    }
+    
+} // Mapile
